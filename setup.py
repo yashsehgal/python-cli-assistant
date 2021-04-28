63231e616e486887ded1c0eed5e50c8a7f9dcce9
yashsehgal <yashsehgal.work@gmail.com>
2021-04-28 15:56:26 +0530

    setup module added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import src.app
 
 APPLICATION_SETUP = src.app.application
 APPLICATION_SETUP(
-  username="yash",
+  username="",      # enter your username here
   app_name="pluto",
   application_runner_command="py-cli run application"
 )
